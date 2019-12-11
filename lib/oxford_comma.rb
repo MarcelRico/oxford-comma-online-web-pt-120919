29376383825dd66e59596cb67612f7b43aee812b
Marcel Rico <personal.mr4@gmail.com>
2019-12-11 16:22:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@ def  oxford_comma(array)
     return array.join()
   elsif(array.length == 2)
     return array.join(" and ")
-  
+  else
+    array.length.times do |idx|
+      if(idx + 1 == )
+    end
   end
 end
