4355ad040e79a43e14916ba923c4be4f34b18d61
Marcel Rico <personal.mr4@gmail.com>
2019-12-11 16:13:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  result = ""
+  array.length.times do |idx|
+    word = array[idx]
+    if(idx+1 ==  array.length && array.length != 1)
+      result << (" and "+word)
+    elsif(array.length ==  1)
+      result << word
+    else
+      result << ", #{word},"
+    end
+  end
+  result
 end
